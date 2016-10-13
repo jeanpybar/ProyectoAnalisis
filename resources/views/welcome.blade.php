a10bccc875549907c0c00a348bf789528effa3f9
root <root@Jean-Pc>
2016-10-12 22:08:39 -0600

    sdsds

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel jjj
                 </div>
 
                 <div class="links">
