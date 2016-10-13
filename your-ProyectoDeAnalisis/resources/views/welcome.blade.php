af88f23425cdf01b963bde434c10f7ae06d85055
jeanpybar <jeanbarquero_@hotmail.com>
2016-10-13 12:06:25 -0600

    Update welcome.blade.php

--- your-ProyectoDeAnalisis/resources/views/welcome.blade.php
+++ your-ProyectoDeAnalisis/resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel tg
+                    Laravel pancho
                 </div>
 
                 <div class="links">
